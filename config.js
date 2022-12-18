9abaf6afd6fbe2f145f6de87d3b29d9b14c437de
minh122004 <116255590+minh122004@users.noreply.github.com>
2022-12-18 11:54:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Cậu';
+const nameGirl = 'LợnCute(Thu Xinh Gái)';
 const giftUrl = 'http://xinchao.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng cậu đó';
-const contentCard = 'Chúc cậu có một 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng Sinh Nhật^.^';
+const titleCard = 'Gửi tới cậu(Minh Thu)';
+const contentCard = 'Nay là một ngày đặc biệt , ngày này vào 18 năm trước cậu đã bước đến thế giới tươi đẹp này cho đến ngày hôm nay là ngày đánh giấu tuổi 18 của cậu.Chúc cho cậu tuổi mới ngày càng càng xinh gái hơn , luôn tràn ngập niềm vui , luôn nở nụ cười tươi trên đôi môi,hay ăn tróng lớn .Thế nha chúc mừng sinh nhật lợn, một ngày sinh nhật thật vui vẻ!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
